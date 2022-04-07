7a35216565566d70f0bb3939b4c4fb47bb7ff64b
Jordan Sukhnandan <kirazuto7@gmail.com>
2022-04-06 22:08:14 -0400

    implemented user can login

--- Pupple/AppDelegate.swift
+++ Pupple/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "clpV4Le8dJjBH8rLyFrm1cLtYuryYMk8X4a2PgkX"
+            $0.clientKey = "97ncMs0mvpNIyjyTxvMHc7G8oK9HTHlKBt4pFvPU"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
