d85c5445e6ccac805e6d8af491c1c7bbb6106f4d
Shareena <95599919+SRW718@users.noreply.github.com>
2022-04-25 09:35:01 -0400

    edit profile

--- Pupple/ViewControllers/UserProfileViewController.swift
+++ Pupple/ViewControllers/UserProfileViewController.swift
@@ -6,75 +6,102 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
 class UserProfileViewController: UIViewController {
     
     
-    @IBOutlet weak var profileImageView: UIView!
-    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var dogView: UIView!
+    
+    @IBOutlet weak var ownerView: UIView!
     
     
-    @IBOutlet weak var dogImageView: UIImageView!{ didSet { dogImageView.image = dogImage } }
+    @IBOutlet weak var dogImageView: UIImageView!
     
-    @IBOutlet weak var dogNameLabel: UILabel!{ didSet{ dogNameLabel.text = dogName } }
+    @IBOutlet weak var dogNameLabel: UILabel!
     
-    @IBOutlet weak var dogBreedLabel: UILabel!{ didSet{ dogBreedLabel.text = breed } }
+    @IBOutlet weak var dogBreedLabel: UILabel!
     
-    @IBOutlet weak var genderIconView: UIImageView!{ didSet{ genderIconView.image = gender } }
+    @IBOutlet weak var genderIconView: UIImageView!
     
-    @IBOutlet weak var dogLocationLabel: UILabel!{ didSet{ dogLocationLabel.text = location } }
+    @IBOutlet weak var dogLocationLabel: UILabel!
     
-    @IBOutlet weak var dogSizeLabel: UILabel!{ didSet{ dogSizeLabel.text = size } }
+    @IBOutlet weak var dogSizeLabel: UILabel!
     
-    @IBOutlet weak var fixedLabel: UILabel!{ didSet{ fixedLabel.text = fixed } }
+    @IBOutlet weak var fixedLabel: UILabel!
     
-    @IBOutlet weak var vaccinatedLabel: UILabel!{ didSet{ vaccinatedLabel.text = vaccinated } }
+    @IBOutlet weak var vaccinatedLabel: UILabel!
     
+    @IBOutlet weak var ownerNameLabel: UILabel!
     
-    
-    @IBOutlet weak var ownerNameLabel: UILabel!{ didSet{ ownerNameLabel.text = ownerName } }
-    
-    @IBOutlet weak var ownerImageview: UIImageView!{ didSet{ ownerImageview.image = ownerImage } }
+    @IBOutlet weak var ownerImageview: UIImageView!
     
     @IBOutlet weak var aboutLabel: UILabel!{ didSet{aboutLabel.text = ""}}
     
     
-    var dogName: String?
-    var dogImage: UIImage?
-    var ownerImage: UIImage?
-    var location: String?
-    var breed: String?
-    var gender: UIImage?
-    var size: String?
-    var vaccinated: String?
-    var fixed: String?
-    var ownerName: String?
+    
     
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         ownerImageview.layer.cornerRadius = 30
-        // Do any additional setup after loading the view.
+        
+        userParse()
+        
     }
+    
     
 
     override func viewDidAppear(_ animated: Bool) {
-        dogSizeLabel.text = size
-        vaccinatedLabel.text = vaccinated
-        fixedLabel.text = fixed
-        ownerNameLabel.text = ownerName
+        
+        
     }
     
     
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func userParse(){
+        let user = PFUser.current()!
+        
+        let name = user["firstname"] as! String
+        let lastName = user["lastname"] as! String
+        let fullName = name + " " + lastName
+        let doglocation = user["location"] as! String
+        self.ownerNameLabel.text = fullName
+        self.dogLocationLabel.text = doglocation
+        
+        var dogs = [PFObject]()
+        let query = PFQuery(className: "Dog")
+        query.whereKey("ownerid", equalTo: user)
+        query.findObjectsInBackground { dog , error in
+            dogs = dog!
+            for ownerDog in dogs {
+                let dogName = ownerDog["name"] as! String
+                let dogBreed = ownerDog["breed"] as! String
+                let dogSize = ownerDog["size"] as! String
+                self.dogNameLabel.text = dogName
+                self.dogBreedLabel.text = dogBreed
+                self.dogSizeLabel.text = dogSize
+                
+                let file = ownerDog["dog_photo"] as! PFFileObject
+                let url = file.url!
+                let dogUrl = URL(string: url)
+                self.dogImageView.af.setImage(withURL: dogUrl!)
+                
+            }
+        }
+        
+        
     }
-    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
