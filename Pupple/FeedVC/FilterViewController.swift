04f2776acb33828f6188eef3fa22c27c72aeab19
Jordan Sukhnandan <kirazuto7@gmail.com>
2022-05-27 12:27:59 -0400

    added size filter and changed filter view to modal

--- Pupple/FeedVC/FilterViewController.swift
+++ Pupple/FeedVC/FilterViewController.swift
@@ -11,35 +11,43 @@ import Alamofire
 class FilterViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
  
     // Keyboard Options
-    let genders = ["","Male", "Female"]
+    let genders = ["None","Male", "Female"]
+    let sizes = ["None", "Small", "Medium", "Large"]
     var breedDictionary = DogBreed(message: ["":[""]])
     var breedArray : [String] = []
     
     // Picker Views
     var genderPickerView = UIPickerView()
+    var sizePickerView = UIPickerView()
     var breedPickerView = UIPickerView()
     
     // User Defaults
     let defaults = UserDefaults.standard
     let GENDER_KEY = "gender_key"
+    let SIZE_KEY = "size_key"
     let BREED_KEY = "breed_key"
     let LOCATION_KEY = "location_key"
+    
+    // For Cancel
+    var prevGender: String?
+    var prevBreed: String?
+    var prevLocation: String?
+    var prevSize: String?
 
     @IBOutlet weak var breedTextField: UITextField!
     @IBOutlet weak var genderTextField: UITextField!
     @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var sizeTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.navigationItem.setHidesBackButton(true, animated: false)
-        self.tabBarController?.tabBar.isHidden = true
         initializeUI()
         
         requestBreeds{ data in
             self.breedDictionary = data!
             let array = self.breedDictionary.message!.keys.sorted()
-            self.breedArray.append("")
+            self.breedArray.append("None")
             for breed in array
             {
                 self.breedArray.append(breed.capitalized)
@@ -48,15 +56,6 @@ class FilterViewController: UIViewController, UITextFieldDelegate, UIPickerViewD
         
         updateFilterPreferences()
         defaults.synchronize()
-    }
-
-    @IBAction func goBack(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let tabbarVC = storyboard.instantiateViewController(withIdentifier: "HomeTabBarController") as! UITabBarController
-        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
-        delegate.window?.rootViewController = tabbarVC
-        delegate.window?.makeKeyAndVisible()
-        self.navigationController?.popToRootViewController(animated: true)
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -71,6 +70,10 @@ class FilterViewController: UIViewController, UITextFieldDelegate, UIPickerViewD
         else if pickerView.tag == 2
         {
             return breedArray.count
+        }
+        else if pickerView.tag == 3
+        {
+            return sizes.count
         }
         else
         {
@@ -87,6 +90,10 @@ class FilterViewController: UIViewController, UITextFieldDelegate, UIPickerViewD
         {
             return breedArray[row]
         }
+        else if pickerView.tag == 3
+        {
+            return sizes[row]
+        }
         else
         {
             return ""
@@ -96,37 +103,70 @@ class FilterViewController: UIViewController, UITextFieldDelegate, UIPickerViewD
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if pickerView.tag == 1
         {
-            genderTextField.text = genders[row]
-            defaults.set(genderTextField.text, forKey: GENDER_KEY)
+            if(genders[row] == "None")
+            {
+                genderTextField.text = ""
+            }
+            else
+            {
+                genderTextField.text = genders[row]
+            }
             genderTextField.resignFirstResponder()
         }
         else if pickerView.tag == 2
         {
-            breedTextField.text = breedArray[row]
-            defaults.set(breedTextField.text, forKey: BREED_KEY)
+            if(breedArray[row] == "None")
+            {
+                breedTextField.text = ""
+            }
+            else
+            {
+                breedTextField.text = breedArray[row]
+            }
             breedTextField.resignFirstResponder()
+        }
+        else if pickerView.tag == 3
+        {
+            if(sizes[row] == "None")
+            {
+                sizeTextField.text = ""
+            }
+            else
+            {
+                sizeTextField.text = sizes[row]
+            }
+            sizeTextField.resignFirstResponder()
         }
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        locationTextField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
-        return false
     }
     
     func initializeUI()
     {
         locationTextField.delegate = self
+        sizeTextField.delegate = self
         breedTextField.delegate = self
         genderTextField.delegate = self
         genderPickerView.delegate = self
         genderPickerView.dataSource = self
         breedPickerView.delegate = self
         breedPickerView.dataSource = self
+        sizePickerView.delegate = self
+        sizePickerView.dataSource = self
         genderPickerView.tag = 1
         breedPickerView.tag = 2
+        sizePickerView.tag = 3
         
         genderTextField.inputView = genderPickerView
         breedTextField.inputView = breedPickerView
+        sizeTextField.inputView = sizePickerView
     }
     
     func updateFilterPreferences()
@@ -134,25 +174,83 @@ class FilterViewController: UIViewController, UITextFieldDelegate, UIPickerViewD
         let genderPreference = defaults.string(forKey: GENDER_KEY)
         let breedPreference = defaults.string(forKey: BREED_KEY)
         let locationPreference = defaults.string(forKey: LOCATION_KEY)
+        let sizePreference = defaults.string(forKey: SIZE_KEY)
         
         genderTextField.text = genderPreference
         breedTextField.text = breedPreference
         locationTextField.text = locationPreference
+        sizeTextField.text = sizePreference
+        prevGender = genderPreference
+        prevBreed = breedPreference
+        prevLocation = locationPreference
+        prevSize = sizePreference
     }
     
     @IBAction func resetPreferences(_ sender: Any) {
         defaults.removeObject(forKey: GENDER_KEY)
         defaults.removeObject(forKey: BREED_KEY)
         defaults.removeObject(forKey: LOCATION_KEY)
+        defaults.removeObject(forKey: SIZE_KEY)
         defaults.set("", forKey: GENDER_KEY)
         defaults.set("", forKey: BREED_KEY)
         defaults.set("", forKey: LOCATION_KEY)
+        defaults.set("", forKey: SIZE_KEY)
         updateFilterPreferences()
+        
+        // Navigate to new feed view w/ preferences reset
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let tabbarVC = storyboard.instantiateViewController(withIdentifier: "HomeTabBarController") as! UITabBarController
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
+        delegate.window?.rootViewController = tabbarVC
+        delegate.window?.makeKeyAndVisible()
+        self.navigationController?.popToRootViewController(animated: true)
     }
     
     @IBAction func setLocation(_ sender: Any) {
+        // Check for invalid input eg: whitespaces only
+        let alphanum = NSCharacterSet.alphanumerics
+        let location = locationTextField.text!
+        let range = location.rangeOfCharacter(from: alphanum)
+        if let location = range
+        {
+            print("valid")
+        }
+        else
+        {
+            print("whitespaces")
+            locationTextField.text = ""
+        }
+    }
+    
+    @IBAction func onCancel(_ sender: Any) {
+        defaults.removeObject(forKey: GENDER_KEY)
+        defaults.removeObject(forKey: BREED_KEY)
+        defaults.removeObject(forKey: LOCATION_KEY)
+        defaults.removeObject(forKey: SIZE_KEY)
+        defaults.set(prevBreed, forKey: BREED_KEY)
+        defaults.set(prevGender, forKey: GENDER_KEY)
+        defaults.set(prevLocation, forKey: LOCATION_KEY)
+        defaults.set(prevSize, forKey: SIZE_KEY)
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func onSave(_ sender: Any) {
+        
+        
+        // Indicate the feed view to update according to the new preferences
+        defaults.set(genderTextField.text, forKey: GENDER_KEY)
+        defaults.set(breedTextField.text, forKey: BREED_KEY)
+        defaults.set(sizeTextField.text, forKey: SIZE_KEY)
         let location = locationTextField.text
         defaults.set(location, forKey: LOCATION_KEY)
+        
+        // Navigate to new feed view w/ saved preferences
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let tabbarVC = storyboard.instantiateViewController(withIdentifier: "HomeTabBarController") as! UITabBarController
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
+        delegate.window?.rootViewController = tabbarVC
+        delegate.window?.makeKeyAndVisible()
+        self.navigationController?.popToRootViewController(animated: true)
     }
     
 }
